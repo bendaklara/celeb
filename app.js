3aad6442f47767c3ce5a1d21658d29a76ad866d3
bendaklara <bendaklara@gmail.com>
2018-08-30 11:28:41 +0200

    first commit

--- app.js
+++ app.js
@@ -69,6 +69,22 @@ app.listen(app.get('port'), () => {
 
 module.exports = app;
 
+/*
+ * Use your own validation token. Check that the token used in the Webhook
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 // Serve the options path and set required headers
 app.get('/options', (req, res, next) => {
     let referer = req.get('Referer');
