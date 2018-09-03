51b6bb3c09519b627af0969dd3f507d56f72459b
bendaklara <bendaklara@gmail.com>
2018-09-03 10:54:13 +0200

    first commit

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ function setRoomPreferences(sender_psid) {
                 text: "OK, let's set your room preferences so I won't need to ask for them in the future.",
                 buttons: [{
                     type: "web_url",
-                    url: SERVER_URL + "/options",
+                    url: "https://datadatbot.tk" + "/options",
                     title: "Set preferences",
                     webview_height_ratio: "compact",
                     messenger_extensions: true
